1995711674fc0a3b7dd57a23579a4479fe957263
harsh2k1 <86097511+harsh2k1@users.noreply.github.com>
2021-08-18 13:09:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import pickle
 import joblib
-# def decompress_pickle(file):
-#     model = bz2.BZ2File(file, 'rb')
-#     model = cPickle.load(model)
-#     return model
-# model = decompress_pickle(r'C:\Users\Harshpreet Singh\Desktop\PVT STUFF\Projects\CreditCardDefaultApp\venv\compressed_dt_model.pbz2')
 model = joblib.load(r'C:\Users\Harshpreet Singh\Desktop\PVT STUFF\Projects\CreditCardDefaultApp\venv\RandomForest_tuned_final.pkl')
 st.title('Credit Card Default Predictor')
 bal = st.number_input(' Enter LIMIT_BAL: Amount of given credit in NT dollars (includes individual and family/supplementary credit)')
